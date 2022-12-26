910bba4770a51a3373cc56c0f9c61cc5dc54dcde
xuanet <the.dark.affliction@gmail.com>
2022-12-26 15:11:07 -0500

    Initial Commit

---
+++ TrailsMakingTest/TrailsMakingTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrailsMakingTestApp.swift
+//  TrailsMakingTest
+//
+//  Created by Kevin Xu on 12/26/22.
+//
+
+import SwiftUI
+
+@main
+struct TrailsMakingTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
