a1553a2efe393999599d0744aade4e960d7cbb80
xuanet <the.dark.affliction@gmail.com>
2023-01-14 18:28:23 -0500

    iOS trailsmakingtest

--- TrailsMakingTest/ContentView.swift
+++ TrailsMakingTest/ContentView.swift
@@ -10,12 +10,23 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Button("Tap Here!") {
+                print("designed by kevin")
+            }
+            Toggle(isOn: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Is On@*/.constant(true)/*@END_MENU_TOKEN@*/) {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Label@*/Text("Label")/*@END_MENU_TOKEN@*/
+            }
+            Image("apple")
+                .resizable()
+                .cornerRadius(20)
+                .frame(width: 400, height: 500)
+            Text("A great source of vitamin A")
+                .bold()
+                .italic()
+                .padding()
+            Text("$3.99 per lb")
         }
-        .padding()
+        .padding()          // makes sure the margins are borders
     }
 }
 
